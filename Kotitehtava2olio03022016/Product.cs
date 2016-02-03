41c9bf2809b43124390d625aab7cd678ae5a3a28
Linkka <samilindroos@hotmail.com>
2016-02-03 15:17:02 +0200

    Initial commit.

---
+++ Kotitehtava2olio03022016/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava2olio03022016
+{
+    class Product
+    {
+        // properties
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return "Product: " + Name + " " + Price + "e";
+        }
+
+        // destructor
+        ~Product()
+        {
+            Console.WriteLine("Product object destroyed.");
+        }
+    }
+
+}
---
+++ Kotitehtava2olio03022016/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava2olio03022016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product { Name = "Milk", Price = 0.79 });
+            products.Add(new Product { Name = "Beer", Price = 1.5});
+            products.Add(new Product { Name = "Bread", Price = 0.89});
+            products.Add(new Product { Name = "Ice-Cream", Price = 1.49});
+
+
+            Console.WriteLine("Products in collection:");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.ToString());
+            }
+            Console.ReadLine();
+        }
+        
+}
+}
